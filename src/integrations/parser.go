be7c65ebfd2a7b67b9f1ddb485e82feed8f8a45b
stefink <stefin@kloudone.com>
2022-06-20 15:54:13 +0530

    AK-8868 Added parser for log message

--- src/integrations/parser.go
+++ src/integrations/parser.go
@@ -1,9 +1,21 @@
 package integrations
 
+import (
+	"encoding/json"
+	"time"
+)
+
 type Log struct {
-	date string
+	DataDomainID     string    `json:"DataDomainID"`
+	Database         string    `json:"Database"`
+	Operation        string    `json:"Operation"`
+	OperationDetails string    `json:"OperationDetails"`
+	Timestamp        time.Time `json:"Timestamp"`
+	User             string    `json:"User"`
 }
 
-func ParseLog(log string) Log {
-	panic("UnImplemented!")
+func ParseLog(logdata string) Log {
+	log := Log{}
+	json.Unmarshal([]byte(logdata), &log)
+	return log
 }
