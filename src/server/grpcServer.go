fe2e5cec9936cce3106f7c75882a381cc2ad7731
aloksharma20 <alokkumar.sharma@accuknox.com>
2022-07-11 11:05:50 +0530

    Datasource class tag

--- src/server/grpcServer.go
+++ src/server/grpcServer.go
@@ -42,18 +42,18 @@ func (d *datasourceServer) ListDatasources(in *ds.ListDatasourceRequest, stream 
 	var datasource []*ds.ListAllDatasource
 	//Hardcoded Data
 	dbData := []*ds.ListAllDatasource{
-		{DsId: "1", DsDatadomain: "org1.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545"},
-		{DsId: "2", DsDatadomain: "org2.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdtyrydfgb3645634545"},
-		{DsId: "3", DsDatadomain: "org3.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "5", DsKey: "hjdhgdfgb3645634545"},
-		{DsId: "4", DsDatadomain: "org4.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545"},
-		{DsId: "5", DsDatadomain: "org5.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjfghfgdfgb3645634545"},
-		{DsId: "6", DsDatadomain: "org6.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb36456909gbjgh45"},
-		{DsId: "7", DsDatadomain: "org7.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgbhjghjghj4545"},
-		{DsId: "8", DsDatadomain: "org8.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "5", DsKey: "ghjghjghjghjghj78678678"},
-		{DsId: "9", DsDatadomain: "org9.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545"},
-		{DsId: "10", DsDatadomain: "org10.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "ghjghjghjgh4456fghfgh"},
-		{DsId: "11", DsDatadomain: "org11.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "fghfghfgh4556fghdfhh"},
-		{DsId: "12", DsDatadomain: "org12.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "fghfgy5668ghh5654"},
+		{DsId: "1", DsDatadomain: "org1.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "2", DsDatadomain: "org2.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdtyrydfgb3645634545", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "3", DsDatadomain: "org3.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "5", DsKey: "hjdhgdfgb3645634545", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "4", DsDatadomain: "org4.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "5", DsDatadomain: "org5.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjfghfgdfgb3645634545", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "6", DsDatadomain: "org6.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb36456909gbjgh45", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "7", DsDatadomain: "org7.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgbhjghjghj4545", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "8", DsDatadomain: "org8.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "5", DsKey: "ghjghjghjghjghj78678678", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "9", DsDatadomain: "org9.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "10", DsDatadomain: "org10.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "ghjghjghjgh4456fghfgh", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "11", DsDatadomain: "org11.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "fghfghfgh4556fghdfhh", CreatedAt: "10-07-2022 12:15:34"},
+		{DsId: "12", DsDatadomain: "org12.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "fghfgy5668ghh5654", CreatedAt: "10-07-2022 12:15:34"},
 	}
 	//Send Response to Client
 	datasource = append(datasource, dbData...)
@@ -93,12 +93,12 @@ func (t *tagServer) ListTag(in *tag.ListTagRequest, stream tag.Tag_ListTagServer
 	var tagdata []*tag.TagResponse
 	//Hardcoded Data
 	dbData := []*tag.TagResponse{
-		{TagId: "1", TagName: "PII-3", TagDescription: "Personal Identofiable Information", TagClass: "Postal Address"},
-		{TagId: "2", TagName: "PII", TagDescription: "Personal Identofiable Information", TagClass: "Bank Account"},
-		{TagId: "3", TagName: "GDPR", TagDescription: "General Data Protection Regulation", TagClass: "Credit Card"},
-		{TagId: "4", TagName: "HIPAA", TagDescription: "Portability Insurance And Accountablity Act Payment Card", TagClass: "Health Card"},
-		{TagId: "5", TagName: "PCI-DSS", TagDescription: "Industry Data Security Standard", TagClass: "Bank Account"},
-		{TagId: "6", TagName: "PHI", TagDescription: "Protected Health Information", TagClass: "Drug Enforcement Agency Registration Number"},
+		{TagId: "1", TagName: "PII-3", TagDescription: "Personal Identofiable Information", TagClass: "Postal Address", CreatedAt: "10-07-2022 12:15:34"},
+		{TagId: "2", TagName: "PII", TagDescription: "Personal Identofiable Information", TagClass: "Bank Account", CreatedAt: "10-07-2022 12:15:34"},
+		{TagId: "3", TagName: "GDPR", TagDescription: "General Data Protection Regulation", TagClass: "Credit Card", CreatedAt: "10-07-2022 12:15:34"},
+		{TagId: "4", TagName: "HIPAA", TagDescription: "Portability Insurance And Accountablity Act Payment Card", TagClass: "Health Card", CreatedAt: "10-07-2022 12:15:34"},
+		{TagId: "5", TagName: "PCI-DSS", TagDescription: "Industry Data Security Standard", TagClass: "Bank Account", CreatedAt: "10-07-2022 12:15:34"},
+		{TagId: "6", TagName: "PHI", TagDescription: "Protected Health Information", TagClass: "Drug Enforcement Agency Registration Number", CreatedAt: "10-07-2022 12:15:34"},
 	}
 	//Send Response to Client
 	tagdata = append(tagdata, dbData...)
@@ -138,12 +138,12 @@ func (c *classServer) ListClass(in *class.ListClassRequest, stream class.Class_L
 	var classdata []*class.ClassResponse
 	//Hardcoded Data
 	dbData := []*class.ClassResponse{
-		{ClassId: "1", ClassName: "Indian Moblie Number", ClassDescription: "Indian Moblie Number", ClassTag: "PII-2"},
-		{ClassId: "1", ClassName: "Passport Number", ClassDescription: "Contains Passport Number", ClassTag: "PII"},
-		{ClassId: "1", ClassName: "Social Security Number", ClassDescription: "Contains Social Security Number", ClassTag: "PII"},
-		{ClassId: "1", ClassName: "Drivers License Number", ClassDescription: "Contains Drivers License ID Number", ClassTag: "PII"},
-		{ClassId: "1", ClassName: "Phone Number", ClassDescription: "Contains Phone Number", ClassTag: "PII"},
-		{ClassId: "1", ClassName: "AWS secrets", ClassDescription: "Contains AWS Secrets", ClassTag: "GDPR"},
+		{ClassId: "1", ClassName: "Indian Moblie Number", ClassDescription: "Indian Moblie Number", ClassTag: "PII-2", CreatedAt: "10-07-2022 12:15:34"},
+		{ClassId: "1", ClassName: "Passport Number", ClassDescription: "Contains Passport Number", ClassTag: "PII", CreatedAt: "10-07-2022 12:15:34"},
+		{ClassId: "1", ClassName: "Social Security Number", ClassDescription: "Contains Social Security Number", ClassTag: "PII", CreatedAt: "10-07-2022 12:15:34"},
+		{ClassId: "1", ClassName: "Drivers License Number", ClassDescription: "Contains Drivers License ID Number", ClassTag: "PII", CreatedAt: "10-07-2022 12:15:34"},
+		{ClassId: "1", ClassName: "Phone Number", ClassDescription: "Contains Phone Number", ClassTag: "PII", CreatedAt: "10-07-2022 12:15:34"},
+		{ClassId: "1", ClassName: "AWS secrets", ClassDescription: "Contains AWS Secrets", ClassTag: "GDPR", CreatedAt: "10-07-2022 12:15:34"},
 	}
 	//Send Response to Client
 	classdata = append(classdata, dbData...)
