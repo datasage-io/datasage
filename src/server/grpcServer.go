01a2ad0749dc9648438cf6e8f772f23780c5b847
aloksharma20 <alokkumar.sharma@accuknox.com>
2022-07-11 09:37:35 +0530

    changes in server

--- src/server/grpcServer.go
+++ src/server/grpcServer.go
@@ -30,16 +30,44 @@ type datasourceServer struct {
 
 func (d *datasourceServer) AddDatasources(in *ds.AddDatasourceRequest, stream ds.Datasource_AddDatasourcesServer) error {
 	fmt.Println("Add Datasource Request Data --- ", in)
+	if err := stream.Send(&ds.MessageResponse{Message: "Success"}); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (d *datasourceServer) ListDatasources(in *ds.ListDatasourceRequest, stream ds.Datasource_ListDatasourcesServer) error {
 	fmt.Println("List Datasource Requested Data --- ", in)
+	//To Store Datasource
+	var datasource []*ds.ListAllDatasource
+	//Hardcoded Data
+	dbData := []*ds.ListAllDatasource{
+		{DsId: "1", DsDatadomain: "org1.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545"},
+		{DsId: "2", DsDatadomain: "org2.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdtyrydfgb3645634545"},
+		{DsId: "3", DsDatadomain: "org3.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "5", DsKey: "hjdhgdfgb3645634545"},
+		{DsId: "4", DsDatadomain: "org4.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545"},
+		{DsId: "5", DsDatadomain: "org5.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjfghfgdfgb3645634545"},
+		{DsId: "6", DsDatadomain: "org6.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb36456909gbjgh45"},
+		{DsId: "7", DsDatadomain: "org7.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgbhjghjghj4545"},
+		{DsId: "8", DsDatadomain: "org8.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "5", DsKey: "ghjghjghjghjghj78678678"},
+		{DsId: "9", DsDatadomain: "org9.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "hjdhgdfgb3645634545"},
+		{DsId: "10", DsDatadomain: "org10.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "ghjghjghjgh4456fghfgh"},
+		{DsId: "11", DsDatadomain: "org11.com", DsName: "AWS", DsDescription: "Amazon Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "fghfghfgh4556fghdfhh"},
+		{DsId: "12", DsDatadomain: "org12.com", DsName: "Google", DsDescription: "Google Data Domain", DsType: "MYSQL", DsVersion: "8", DsKey: "fghfgy5668ghh5654"},
+	}
+	//Send Response to Client
+	datasource = append(datasource, dbData...)
+	if err := stream.Send(&ds.ListDatasourceResponse{ListAllDatasources: datasource}); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (d *datasourceServer) DeleteDatasources(in *ds.DeleteDatasourceRequest, stream ds.Datasource_DeleteDatasourcesServer) error {
 	fmt.Println("Delete Datasource Requested Data --- ", in)
+	if err := stream.Send(&ds.MessageResponse{Message: "Success"}); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -53,16 +81,38 @@ type tagServer struct {
 
 func (t *tagServer) AddTag(in *tag.CreateTagRequest, stream tag.Tag_AddTagServer) error {
 	fmt.Println("Add Tag Request Data --- ", in)
+	if err := stream.Send(&tag.TagMessageResponse{Message: "Success"}); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (t *tagServer) ListTag(in *tag.ListTagRequest, stream tag.Tag_ListTagServer) error {
 	fmt.Println("List Tag Requested Data --- ", in)
+	//To Store Tag
+	var tagdata []*tag.TagResponse
+	//Hardcoded Data
+	dbData := []*tag.TagResponse{
+		{TagId: "1", TagName: "PII-3", TagDescription: "Personal Identofiable Information", TagClass: "Postal Address"},
+		{TagId: "2", TagName: "PII", TagDescription: "Personal Identofiable Information", TagClass: "Bank Account"},
+		{TagId: "3", TagName: "GDPR", TagDescription: "General Data Protection Regulation", TagClass: "Credit Card"},
+		{TagId: "4", TagName: "HIPAA", TagDescription: "Portability Insurance And Accountablity Act Payment Card", TagClass: "Health Card"},
+		{TagId: "5", TagName: "PCI-DSS", TagDescription: "Industry Data Security Standard", TagClass: "Bank Account"},
+		{TagId: "6", TagName: "PHI", TagDescription: "Protected Health Information", TagClass: "Drug Enforcement Agency Registration Number"},
+	}
+	//Send Response to Client
+	tagdata = append(tagdata, dbData...)
+	if err := stream.Send(&tag.ListTagResponse{TagResponse: tagdata}); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (t *tagServer) DeleteTag(in *tag.DeleteTagRequest, stream tag.Tag_DeleteTagServer) error {
 	fmt.Println("Delete Tag Requested Data --- ", in)
+	if err := stream.Send(&tag.TagMessageResponse{Message: "Success"}); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -76,16 +126,38 @@ type classServer struct {
 
 func (c *classServer) AddClass(in *class.CreateClassRequest, stream class.Class_AddClassServer) error {
 	fmt.Println("Add Class Request Data --- ", in)
+	if err := stream.Send(&class.ClassMessageResponse{Message: "Success"}); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (c *classServer) ListClass(in *class.ListClassRequest, stream class.Class_ListClassServer) error {
 	fmt.Println("List Class Requested Data --- ", in)
+	//To Store Class
+	var classdata []*class.ClassResponse
+	//Hardcoded Data
+	dbData := []*class.ClassResponse{
+		{ClassId: "1", ClassName: "Indian Moblie Number", ClassDescription: "Indian Moblie Number", ClassTag: "PII-2"},
+		{ClassId: "1", ClassName: "Passport Number", ClassDescription: "Contains Passport Number", ClassTag: "PII"},
+		{ClassId: "1", ClassName: "Social Security Number", ClassDescription: "Contains Social Security Number", ClassTag: "PII"},
+		{ClassId: "1", ClassName: "Drivers License Number", ClassDescription: "Contains Drivers License ID Number", ClassTag: "PII"},
+		{ClassId: "1", ClassName: "Phone Number", ClassDescription: "Contains Phone Number", ClassTag: "PII"},
+		{ClassId: "1", ClassName: "AWS secrets", ClassDescription: "Contains AWS Secrets", ClassTag: "GDPR"},
+	}
+	//Send Response to Client
+	classdata = append(classdata, dbData...)
+	if err := stream.Send(&class.ListClassResponse{ClassResponse: classdata}); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (c *classServer) DeleteClass(in *class.DeleteClassRequest, stream class.Class_DeleteClassServer) error {
 	fmt.Println("Delete CLass Requested Data --- ", in)
+	if err := stream.Send(&class.ClassMessageResponse{Message: "Success"}); err != nil {
+		return err
+	}
 	return nil
 }
 
