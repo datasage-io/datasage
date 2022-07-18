98e8ae6b769d615085f48681827cb7b0a1713708
Arul Jeyananth James Edison <arul.jeyananth@kloudone.com>
2022-07-18 20:38:27 +0530

    added periodic scan
    added periodic scan

--- src/classifiers/classfication_handler.go
+++ src/classifiers/classfication_handler.go
@@ -2,12 +2,17 @@ package classifiers
 
 import (
 	"fmt"
-	"log"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/datasage-io/datasage/src/adaptors"
 	"github.com/datasage-io/datasage/src/storage"
+
+	logger "github.com/datasage-io/datasage/src/logger"
+	"github.com/rs/zerolog"
+
+	"github.com/spf13/viper"
 )
 
 type DpDataSource struct {
@@ -23,195 +28,142 @@ type DpDataSource struct {
 	Password     string `json:"Password"`
 }
 
-func Run(dpDataSource DpDataSource) {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	log.Println("Classification Handler Run")
-	//Fetch MetaData
-	adpt, err := adaptors.New(adaptors.AdaptorConfig{
-		Type:     dpDataSource.Dstype,
-		Username: dpDataSource.User,
-		Password: dpDataSource.Password,
-		Host:     dpDataSource.Host})
+var log *zerolog.Logger = logger.GetInstance()
 
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	info, err := adpt.Scan()
+func Run() {
 
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-
-	if err != nil {
-		log.Println(err.Error())
-	}
-
-	st, err := storage.New(storage.StorageConfig{Type: "internal", Path: "datasageD.db"})
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	//get all classes
-	classes, err := st.GetClasses()
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-	log.Println("no of classes:= ", len(classes))
-
-	//get all tags
-	tags, err := st.GetTags()
-	if err != nil {
-		log.Println(err.Error())
-	}
-	log.Println("no of tags:= ", len(tags))
-
-	storageDpDataSourceObj := storage.DpDataSource{
-		ID:           -1,
-		Datadomain:   dpDataSource.Datadomain,
-		Dsname:       dpDataSource.Dsname,
-		Dsdecription: dpDataSource.Dsdecription,
-		Dstype:       dpDataSource.Dstype,
-		DsKey:        dpDataSource.DsKey,
-		Dsversion:    dpDataSource.Dsversion,
-		Host:         dpDataSource.Host,
-		Port:         dpDataSource.Port,
-		User:         dpDataSource.User,
-		Password:     dpDataSource.Password,
-	}
-
-	err1 := st.SetDpDataSourceData(storageDpDataSourceObj)
-	if err1 != nil {
-		fmt.Println("SetDpDataSourceData error: ", err)
-		return
-	}
-
-	/*
-		phonetag, err := st.GetAssociatedTags("Phone Number")
+	scanInterval := viper.GetInt("classifiers.dbschema-scan-interval")
+	ticker := time.NewTicker(time.Duration(scanInterval) * time.Minute)
+	for ; ; <-ticker.C {
+		fmt.Println("starting a scan")
+		st, err := storage.GetStorageInstance()
 		if err != nil {
-			log.Println(err.Error())
-		}
-		log.Println(phonetag)
-	*/
-	log.Println(" identifying class and tags start")
-	columsCount := 0
-	tableCount := 0
-	dBCount := 0
-	skipDBs := []string{"mysql", "performance_schema", "datadefender"}
-
-	for _, sc := range info.Schemas {
-		dBCount = dBCount + 1
-		log.Println("DB name:= ", sc.Name)
-		skip := false
-		for _, skipDB := range skipDBs {
-
-			if skipDB == sc.Name {
-				log.Println("skip DB name:= ", sc.Name)
-				skip = true
-			}
-		}
-		if skip == true {
+			log.Error().Err(err).Msg("Internal Error")
 			continue
 		}
 
-		dpDbTables := []storage.DpDbTable{}
-		for _, tb := range sc.Tables {
-			tableCount = tableCount + 1
-			//dpDbColumn := storage.DpDbColumn
+		datasources, err := st.GetDataSources()
+		if err != nil {
+			fmt.Println("ListDatasources error  ")
+		}
+		for _, datasource := range datasources {
+			fmt.Println(datasource)
 
-			dpDbColumns := []storage.DpDbColumn{}
+			adaptor, err := adaptors.New(adaptors.AdaptorConfig{
+				Type:     datasource.Dstype,
+				Username: datasource.User,
+				Password: datasource.Password,
+				Host:     datasource.Host,
+			})
 
-			for _, cols := range tb.Cols {
-				columsCount = columsCount + 1
-				colName, err1 := removeSpecialChars(cols.ColumnName)
-				if err1 != nil {
-					log.Println(err.Error())
+			if err != nil {
+				log.Error().Err(err).Msg("Internal Error")
+				continue
+			}
+
+			//get all classes
+			classes, err := st.GetClasses()
+			if err != nil {
+				log.Error().Err(err).Msg("Internal Error")
+
+			}
+			log.Info().Msgf("no of classes:=%v ", len(classes))
+
+			//get all tags
+			tags, err := st.GetTags()
+			if err != nil {
+				log.Error().Err(err).Msg("Internal Error")
+			}
+			log.Info().Msgf("no of tags:=%v ", len(tags))
+			columsCount := 0
+			tableCount := 0
+			dBCount := 0
+			skipDBs := []string{"mysql", "performance_schema", "datadefender"}
+			info, err := adaptor.Scan()
+			for _, sc := range info.Schemas {
+				dBCount = dBCount + 1
+				log.Info().Msgf("DB name:= %v", sc.Name)
+				skip := false
+				for _, skipDB := range skipDBs {
+
+					if skipDB == sc.Name {
+						log.Info().Msgf("skip DB name:= ", sc.Name)
+						skip = true
+					}
+				}
+				if skip == true {
 					continue
 				}
-				relatedtags, _ := st.GetAssociatedTags(colName)
-				relatedclasses, _ := st.GetAssociatedClasses(colName)
 
-				//if err != nil {
-				//	log.Println(err.Error())
-				//}
-				tags := []string{}
-				classes := []string{}
-				if len(relatedclasses) > 0 {
-					for _, relatedclass := range relatedclasses {
-						log.Println("Class:= ", relatedclass.Class)
-						//classes = classes + ";" + relatedclass.
-						classes = append(classes, relatedclass.Class)
+				dpDbTables := []storage.DpDbTable{}
+				for _, tb := range sc.Tables {
+					tableCount = tableCount + 1
+					//dpDbColumn := storage.DpDbColumn
+
+					dpDbColumns := []storage.DpDbColumn{}
+
+					for _, cols := range tb.Cols {
+						columsCount = columsCount + 1
+						colName, err := removeSpecialChars(cols.ColumnName)
+						if err != nil {
+							log.Error().Err(err).Msg("Internal Error")
+							continue
+						}
+						relatedtags, _ := st.GetAssociatedTags(colName)
+						relatedclasses, _ := st.GetAssociatedClasses(colName)
+
+						//if err != nil {
+						//	log.Println(err.Error())
+						//}
+						tags := []string{}
+						classes := []string{}
+						if len(relatedclasses) > 0 {
+							for _, relatedclass := range relatedclasses {
+								log.Info().Msgf("Class:= %v", relatedclass.Class)
+								classes = append(classes, relatedclass.Class)
+							}
+						}
+						if len(relatedtags) > 0 {
+							for _, relatedtag := range relatedtags {
+								log.Info().Msgf("TagName:= %v", relatedtag.TagName)
+								//tags = tags + ";" + relatedtag.TagName
+								tags = append(tags, relatedtag.TagName)
+							}
+						}
+						col := storage.DpDbColumn{
+							ColumnName:    colName,
+							ColumnType:    cols.ColumnType,
+							ColumnComment: cols.ColumnComment,
+							Tags:          strings.Join(tags, ";"),
+							Classes:       strings.Join(classes, ";"),
+						}
+						dpDbColumns = append(dpDbColumns, col)
 					}
-				}
-				if len(relatedtags) > 0 {
-					for _, relatedtag := range relatedtags {
-						log.Println("TagName:", relatedtag.TagName)
-						//tags = tags + ";" + relatedtag.TagName
-						tags = append(tags, relatedtag.TagName)
+					dpDbTable := storage.DpDbTable{
+
+						Name:        tb.Name,
+						Tags:        "",
+						DpDbColumns: dpDbColumns,
 					}
+					dpDbTables = append(dpDbTables, dpDbTable)
+
 				}
-				col := storage.DpDbColumn{
-					ColumnName:    colName,
-					ColumnType:    cols.ColumnType,
-					ColumnComment: cols.ColumnComment,
-					Tags:          strings.Join(tags, ";"),
-					Classes:       strings.Join(classes, ";"),
+				schema := storage.DpDbDatabase{
+					DsKey:      datasource.DsKey,
+					Name:       sc.Name,
+					Type:       datasource.Dstype,
+					DpDbTables: dpDbTables,
 				}
-				dpDbColumns = append(dpDbColumns, col)
-			}
-			dpDbTable := storage.DpDbTable{
+				err := st.SetSchemaData(schema)
+				if err != nil {
+					fmt.Println(err)
+				}
 
-				Name:        tb.Name,
-				Tags:        "",
-				DpDbColumns: dpDbColumns,
 			}
-			dpDbTables = append(dpDbTables, dpDbTable)
-
-		}
-		schema := storage.DpDbDatabase{
-			DbKey:      "todo",
-			Name:       sc.Name,
-			Type:       "mysql",
-			Key:        "987654321",
-			DpDbTables: dpDbTables,
-		}
-		err := st.SetSchemaData(schema)
-		if err != nil {
-			fmt.Println(err)
+			log.Info().Msgf("scan completed for datasource: %v", datasource.Host)
 		}
 	}
 
-	log.Println(" identifying class and tags completed")
-	log.Println(" columsCount", columsCount)
-
-}
-
-func ListDatasources() ([]storage.DpDataSource, error) {
-	log.Println("ListDatasources")
-	st, err := storage.New(storage.StorageConfig{Type: "internal", Path: "datasageD.db"})
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	dataSources, err := st.GetDpDataSources()
-	log.Println(dataSources)
-	return dataSources, err
-
-}
-
-func DeleteDatasource(ids []int64) bool {
-	overallStatus := true
-	log.Println("DeleteDatasources ")
-	st, err := storage.New(storage.StorageConfig{Type: "internal", Path: "datasageD.db"})
-	if err == nil {
-		for i := range ids {
-			status, error := st.DeleteDpDataSources(ids[i])
-			if error != nil || status == false {
-				log.Println(error)
-				overallStatus = false
-			}
-		}
-	}
-	return overallStatus
 }
 
 //removeSpecialChars - Removes the special characters from the string.
