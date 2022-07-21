fecdc2b4061435dc35f1c6a94f5020c4e5fec0a5
stefink <105198644+stefink@users.noreply.github.com>
2022-07-21 13:42:33 +0530

    Script to download latest binary to path

--- install.sh
+++ install.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
+latest_datasage_download_url(){
+	curl -s https://api.github.com/repos/datasage-io/datasage/releases/latest \
+		| grep "browser_download_url.*tar.gz" \
+		| cut -d : -f 2,3 \
+		| tr -d \"
+}
 
-download_binary(){
-	curl -L -o datasage.tar.gz https://github.com/datasage-io/datasage/releases/download/0.1.0/datasage_0.1.0_linux_amd64.tar.gz
+latest_datasage_cli_download_url(){
+	curl -s https://api.github.com/repos/datasage-io/datasage-cli/releases/latest \
+		| grep "browser_download_url.*tar.gz" \
+		| cut -d : -f 2,3 \
+		| tr -d \"
+}
+
+download_datasage(){
+	echo "Downloading latest datasage release..."
+	curl -s -L -o datasage.tar.gz $(latest_datasage_download_url)
+	echo "Downloading datasage version {$1}"
 	tar -xvf datasage.tar.gz
 	mv datasage /usr/local/bin/
 }
+
+download_datasage_cli(){
+	echo "Downloading latest datasage-cli..."
+	curl -s -L -o datasage-cli.tar.gz $(latest_datasage_cli_download_url) 
+	tar -xvf datasage-cli.tar.gz
+	mv datasage-cli /usr/local/bin/
+}
+
 download_csv(){
-	curl -L -o class.csv https://raw.githubusercontent.com/datasage-io/datasage/main/src/storage/default/class.csv
-	curl -L -o tags.csv https://raw.githubusercontent.com/datasage-io/datasage/main/src/storage/default/tags.csv
+	echo "Downloading csv files..."
+	curl -s -L -o class.csv https://raw.githubusercontent.com/datasage-io/datasage/main/src/storage/default/class.csv
+	curl -s -L -o tags.csv https://raw.githubusercontent.com/datasage-io/datasage/main/src/storage/default/tags.csv
 }
 
 download_config(){
-	curl -L -o datasage.yaml https://raw.githubusercontent.com/datasage-io/datasage/main/src/conf/datasage.yaml
+	echo "Downloading config files..."
+	curl -s -L -o datasage.yaml https://raw.githubusercontent.com/datasage-io/datasage/main/src/conf/datasage.yaml
 }
 
 copy_csv_to_resources(){
 	mkdir -p /etc/datasage/resources/
-	mv *.csv /etc/datasage/resources/
+	mv class.csv /etc/datasage/resources/
+	mv tags.csv /etc/datasage/resources/
 }
 
 copy_config_to_path(){
@@ -25,9 +51,15 @@ copy_config_to_path(){
 	mv datasage.yaml /etc/datasage/conf/
 }
 
+cleanup(){
+	rm datasage.tar.gz datasage-cli.tar.gz 
+	echo "Installation complete!"
+}
 
-download_binary
+download_datasage
+download_datasage_cli
 download_csv
-copy_csv_to_resources
 download_config
+copy_csv_to_resources
 copy_config_to_path
+cleanup
